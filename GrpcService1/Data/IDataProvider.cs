874d2445fb5c07a60ae7c731f9dfd55c238cc129
GinessisC <106706657+GinessisC@users.noreply.github.com>
2025-08-02 22:06:14 +0200

    Delete GrpcService1 directory

--- GrpcService1/Data/IDataProvider.cs
+++
@@ -1,9 +0,0 @@
-using GrpcService1.Models;
-
-namespace GrpcService1.Data;
-
-public interface IDataProvider
-{
-	Task<CustomerDto> GetCustomer(int customerId);
-	Task<IList<CustomerDto>> GetAllCustomersAsync();
-}
